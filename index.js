cb1f846f47e53af445b0ba6a565bcfb416f2929e
BrayOA <borconarellano@gmail.com>
2023-10-13 01:06:49 -0500

    sendStatus

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
 const express = require("express");
 const app = express();
 
-    app.get("/", (req, res) => {
-    res.send("Hola mundo");
-    });
+app.get("/", (req, res) => {
+  res.send("hola");
+});
 
-    app.get("/about", (req, res) => {
-    res.send("About");
-    });
+app.get("/miarchivo", (req, res) => {
+  res.sendFile("./logoBOA-black.png", {
+    root: __dirname,
+  });
+});
 
-    app.get("/weater", (req, res) => {
-    res.send("The current weather is Nice");
-    });
+app.get("/user", (req, res) => {
+  res.json({
+    name: "bray",
+    lastname: "orcon arellano",
+    age: 23,
+    points: [1, 2, 3, 4],
+    adress: {
+      city: "chupaca",
+      street: "HuamanCC-Chico",
+    },
+  });
+});
 
-    app.use((req, res) => {
-        res.status(404).send('Error 404. Nose encontro tu pagina')
-    })
+app.get("/isAlive", (req, res) => {
+    res.sendStatus(204);
+  });    
 
 app.listen(3000);
 console.log(`Server on port ${3000}`);
