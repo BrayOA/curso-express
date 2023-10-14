3f4526f6a30f12a2d63159c283c23f63816bc3c7
BrayOA <borconarellano@gmail.com>
2023-10-14 03:26:12 -0500

    meodo get

--- index.js
+++ index.js
@@ -1,32 +1,42 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("hola");
+app.al("/info", (req, res) => {
+  res.send("server info");
 });
 
-app.get("/miarchivo", (req, res) => {
-  res.sendFile("./logoBOA-black.png", {
-    root: __dirname,
-  });
+app.get("/search", (req, res) => {
+  if (req.query.q === "javascript books") {
+    res.send("lista de libros de js");
+  } else {
+    res.send("pagina normal");
+  }
 });
 
-app.get("/user", (req, res) => {
-  res.json({
-    name: "bray",
-    lastname: "orcon arellano",
-    age: 23,
-    points: [1, 2, 3, 4],
-    adress: {
-      city: "chupaca",
-      street: "HuamanCC-Chico",
-    },
-  });
+app.get("/hello/:username", (req, res) => {
+  res.send(`hello ${req.params.username.toUpperCase()}`);
 });
 
-app.get("/isAlive", (req, res) => {
-    res.sendStatus(204);
-  });    
+app.get("/add/:x/:y", (req, res) => {
+  const { x, y } = req.params;
+  res.send(`Result: ${parseInt(x) + parseInt(y)}`);
+});
+
+app.get("/users/:usernames/photo", (req, res) => {
+  console.log(req.params);
+  if (req.params.usernames === "fazt") {
+    return res.sendFile("./logoBOA-black.png", {
+      root: __dirname,
+    });
+  }
+
+  res.send("el usuario no tiene acceso");
+});
+
+app.get("/name/:nombre/age/:age", (req, res) => {
+  console.log(req.params);
+  res.send(`El usuario ${req.params.nombre} tiene ${req.params.age} años`);
+});
 
 app.listen(3000);
 console.log(`Server on port ${3000}`);
